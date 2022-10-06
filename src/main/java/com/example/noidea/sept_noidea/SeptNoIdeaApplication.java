233e389cf62a0348cb07e83915bd61c337e35b16
Kuma <49276394+soeviturek@users.noreply.github.com>
2022-10-07 01:13:12 +0800

    Add import

--- src/main/java/com/example/noidea/sept_noidea/SeptNoIdeaApplication.java
+++ src/main/java/com/example/noidea/sept_noidea/SeptNoIdeaApplication.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @EnableJpaAuditing
 @SpringBootApplication
